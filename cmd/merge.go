189e94431bad7bbf10117c02efef695cba927ffe
brendreyes <brendreyes1234@gmail.com>
2026-04-22 20:38:42 +0800

    feat: implemented convert logic, only image to pdf

--- cmd/merge.go
+++ cmd/merge.go
@@ -15,47 +15,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-/**
-TODO:
-[x] Remove the overwriting a same file name
-[x] Implement ability to choose a directory to store the file
-[x] Auto naming
-[x] Summary output
-[x] Check if file already exist
-**/
-
-/**
-EDGE CASES TO CHECK
-Input files (args):
-
-[x]Valid .pdf files → no error
-[x]A .png file in args → error
-[x]A .docx file in args → error
-[x]A file with no extension → error
-[x]A file with .PDF uppercase extension → interesting, does your check catch it?
-[x]Empty string as a filename → error
-[x]Full path with valid .pdf → no error e.g. /Users/john/file.pdf
-[x]Full path with invalid extension → error e.g. /Users/john/file.png
-
-Output filename:
-
-[x]Valid result.pdf → no error
-[x]No extension result → error
-[x]Wrong extension result.png → error
-[x].pdf uppercase result.PDF → does it catch it?
-[x]Just .pdf with no base name → edge case worth knowing
-[x]Output with full path /Users/john/result.pdf → how does it behave with --dir combined?
-
-Directory:
-
-[x]Valid existing directory → no error
-[x]Non-existing directory → error
-[x]A file path instead of directory → error
-[x]Empty string → error
-[x]. current directory → no error
-[x].. parent directory → no error
-**/
-
 // mergeCmd represents the merge command
 var mergeCmd = &cobra.Command{
 	Use:     "merge <file1.pdf> <file2.pdf> *use quotation for filename or directory with spaces or special char* [more.pdf...]",
